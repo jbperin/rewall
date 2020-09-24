bab06cd3e89610552c96f63910c5964a60cdb791
jbperin <jbperin@users.noreply.github.com>
2020-09-24 17:07:23 +0200

    try to fix and fasten a little bit

--- tools/genTabCi.py
+++ tools/genTabCi.py
@@ -5,7 +5,7 @@ from table import CosTable
 def main():
     for ii in range (-20, 20, 1):
         # v=((2**18)/(CosTable [abs(ii)]*2))
-        v=(2**19)/(CosTable [abs(ii)])
+        v=(2**18)/(CosTable [abs(ii)])
         print (",", round(v))
 
 if __name__ == '__main__':
